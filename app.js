9296bbf717dcf46f125b10c564babf2f0baf209d
Jeremy Gann <56943728+Jgann123@users.noreply.github.com>
2020-12-22 11:57:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var PORT = 3000;
+var PORT = process.environment.port;
 
 
 var indexRouter = require('./routes/index');
